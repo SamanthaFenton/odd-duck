712a3a7d1e1c00d71e5b8ffa75540ee8d8dc8e8d
Samantha <samantha.fenton@live.co.uk>
2023-06-26 16:15:33 +0100

    save to prevent js loss of code

--- app.js
+++ app.js
@@ -1,2 +1,59 @@
 "use strict";
 console.log("strict");
+
+const productContainer = document.querySelector("section");
+const resultsButton = document.querySelector("section + div");
+
+const image1 = document.querySelector("section img:first-child");
+const image2 = document.querySelector("section img:nth-child(2)");
+const image3 = document.querySelector("section img:nth-child(3)");
+
+let clicks = 0;
+console.log("clicks");
+
+let maxClicksAllowed = 5;
+
+let allProducts = [];
+
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  this.clicks = 0;
+  this.views = 0;
+  allProducts.push(this);
+}
+
+function renderProducts() {
+  let product1 = getRandomNumber();
+  let product2 = getRandomNumber();
+  let product3 = getRandomNumber();
+
+  while (product1 === product2) {
+    product2 = getRandomNumber();
+    // cannot do this without help as I dont know how to rotate 3 options
+  }
+
+  image1.src = allProducts[product1].src;
+  image2.src = allProducts[product2].src;
+  image3.src = allProducts[product3].src;
+  image1.alt = allProducts[product1].name;
+  image2.alt = allProducts[product2].name;
+  image3.alt = allProducts[product3].name;
+  allProducts[product1].views++;
+  allProducts[product2].views++;
+  allProducts[product3].views++;
+}
+
+function handleProductClick(event) {
+  if (event.target === productContainer) {
+    alert("Please click on the image");
+  } else {
+    clicks++;
+    let clickedProduct = event.target.alt;
+    for (let i = 0; i < allProducts.length; i++) {
+      if (clickedProduct === allProducts[i].name) {
+        allProducts[i].clicks;
+      }
+    }
+  }
+}
